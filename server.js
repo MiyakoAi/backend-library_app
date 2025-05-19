9ca364a4db05d48ffe41dda7550a3d9940bd772d
MiyakoAi <miyako.key25@gmail.com>
2025-05-19 20:43:45 +0800

    test deploy

--- server.js
+++ server.js
@@ -1,38 +1,20 @@
-// server.js
-import app from './src/app.js';
-import sequelize from './src/config/db.js';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import app from "./src/app.js";
+import sequelize from "./src/config/db.js";
 
 const PORT = process.env.PORT || 8080;
 
 const startServer = async () => {
   try {
     await sequelize.authenticate();
-    console.log('Database terkonek');
-
     await sequelize.sync();
-    console.log('models tersingkronisasi.');
+    console.log("Database connected and synced successfully.");
 
     app.listen(PORT, () => {
-      
-      let count = 0;
-      const interval = setInterval(() => {
-        count = (count % 5) + 1;
-        const dots = '.'.repeat(count);
-        process.stdout.write(`\rLoading${dots}   `);
-      }, 500);
-      setTimeout(() => {
-        clearInterval(interval);
-        console.log(`\rServer is running at http://localhost:${PORT}\n`);
-        process.stdout.write("\rHello Sekai! >_<     \n");
-      }, 5000);
-
+      console.log(`Server is running at http://localhost:${PORT}`);
+      console.log("Hello Sekai!");
     });
-
   } catch (error) {
-    console.error('tidak dapat menyambungkan ke database:', error.message);
+    console.error("Unable to connect to the database:", error);
     process.exit(1);
   }
 };
